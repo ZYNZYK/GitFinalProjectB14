e0c4b923b3f03ef93a01592230508360de3cf214
ZYNZYK <zinaidacantir@gmail.com>
2022-09-28 20:11:02 -0400

    first commit

---
+++ src/class01Codes/Class01Codes.java
@@ -0,0 +1,10 @@
+package class01Codes;
+
+public class Class01Codes {
+	public static void main(String[] args) {
+		// code to launch the nuclear weapon
+		System.out.println("Launch my favorite TV Show ");
+	}
+	
+
+}
